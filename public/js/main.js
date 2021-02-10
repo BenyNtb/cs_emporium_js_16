2cb5b979c378d48fa8e84cabc9ed8ef3b7c412d1
Beny <b.ntambwe97@gmail.com>
2021-02-10 13:29:43 +0100

    fonctions dark mode + light mode

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,40 @@
-// Début Bouton connection
+let dark = document.querySelector(".black")
+let light = document.querySelector(".blanc")
+let emporium = document.querySelector(".empo")
+let navbar = document.querySelectorAll(".nav-link")
+let recents = document.getElementById("rp")
+let pararecents = document.querySelectorAll("#para1")
+let discover = document.getElementById("discover")
+console.log(dark);
+console.log(light);
+
+// CHANGEMENT DE THEME
+
+     // DARK MODE
+dark.addEventListener('click', () => {
+    document.body.style.backgroundColor = "black";
+    emporium.style.color = "white";
+    navbar.forEach(element => {
+        element.style.color = "white"
+    });
+    recents.style.color = "white";
+    pararecents.forEach(element => {
+        element.style.color = "white"
+    });
+    discover.style.color = "white"
+    // pararecents.style.color = "white";
+})
+
+        // LIGHT MODE
+light.addEventListener('click', () => {
+    document.body.style.backgroundColor = "white"
+    emporium.style.color = "black"
+    navbar.forEach(element => {
+        element.style.color = "black"
+    });
+    recents.style.color = "black";
+    pararecents.forEach(element => {
+        element.style.color = "black"
+    });
+    discover.style.color = "black"
+})
