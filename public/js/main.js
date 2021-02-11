7899404595e03a9d47c54d28b2b15abfeef6741f
Beny <b.ntambwe97@gmail.com>
2021-02-11 10:44:30 +0100

    modification modale + style

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+// const { Button } = require("bootstrap")
+
 let dark = document.querySelector(".black")
 let light = document.querySelector(".blanc")
 let emporium = document.querySelector(".empo")
@@ -37,4 +39,42 @@ light.addEventListener('click', () => {
         element.style.color = "black"
     });
     discover.style.color = "black"
+})
+
+// CONNEXION / SUBSCRIPTION
+
+let connexion = document.querySelector('#modale1')
+let inscription = document.querySelector('#modale2')
+let btnConnexion = document.querySelector('.connexion')
+let btnSubs = document.querySelector('.inscription')
+let btnSign = document.querySelector('.sign')
+let popUp = document.querySelector('#connexion')
+let btnClose = document.querySelector('.fermer')
+
+    // CONNEXION
+
+btnConnexion.addEventListener('click', () => {
+    if (connexion.classList.contains('d-none')) {
+        connexion.classList.remove('d-none')
+    }
+    inscription.classList.add('d-none')
+})   
+btnSign.addEventListener('click', () => {
+    popUp.style.display = "block"
+})
+btnClose.addEventListener('click', () => {
+    popUp.style.display = "none"
+})
+btnSign.addEventListener('click', () => {
+    if (connexion.classList.contains('d-none')) {
+        connexion.classList.remove('d-none')
+    }
+    inscription.classList.add('d-none')
+})
+
+btnSubs.addEventListener('click', () => {
+    if (inscription.classList.contains('d-none')) {
+        inscription.classList.remove('d-none')
+    }
+    connexion.classList.add('d-none')
 })
